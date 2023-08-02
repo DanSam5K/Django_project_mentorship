b391bdbd81848fd730d473d2b5a6aef8fff280c7
Daniel Samuel <dansam5000@gmail.com>
2023-08-02 14:10:22 +0200

    Add dynamic pages with class based views

--- home/views.py
+++ home/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.now()})
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/welcome.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['today'] = datetime.now()
+        return context
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
+class AuthorizedView(TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,30 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
 
+
+from .models import Notes
 # Create your views here.
+
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
+
+    def get_queryset(self):
+        return Notes.objects.all()
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+    template_name = 'notes/notes_detail.html'
+
+    def get_queryset(self):
+        return Notes.objects.all()
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#         return render(request, 'notes/notes_detail.html', {'note': note})
+#     except Notes.DoesNotExist:
+#         return HttpResponse('Note not found')
